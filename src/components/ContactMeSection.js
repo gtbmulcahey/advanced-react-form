516dd05d95a2cb7ff50c414f580dec2f643bc7c8
gtbmulcahey <129100734+GailMulcaheyRBA@users.noreply.github.com>
2023-04-04 10:44:46 -0500

    Initial Add

---
+++ src/components/ContactMeSection.js
@@ -0,0 +1,138 @@
+import React, { useEffect } from "react";
+import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import * as Yup from 'yup';
+import FullScreenSection from "./FullScreenSection";
+import useSubmit from "../hooks/useSubmit";
+import { useAlertContext } from "../context/alertContext";
+
+const LandingSection = () => {
+  const { isLoading, response, submit } = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      email: '',
+      type: 'hireMe',
+      comment: ''
+    },
+    onSubmit: (values) => {
+      //alert(JSON.stringify(values, null, 2));
+      submit("someUrl", values);
+    },
+    validationSchema: Yup.object().shape({
+      firstName: Yup.string()
+        .required("Required"),
+      email: Yup.string()
+        .required("Required"),
+      comment: Yup.string()
+        .required("Required")
+    }),
+  });
+
+  useEffect(() => {
+    console.log({ formik });
+    console.log({ onOpen });
+    console.log({ response });
+    if(response) {
+      onOpen(response.type, response.message);
+      if(response.type === "success") {
+        formik.resetForm();
+      }
+    }  
+  }, [response]);
+
+  return (
+    <FullScreenSection
+      isDarkBackground
+      backgroundColor="#512DA8"
+      py={16}
+      spacing={8}
+    >
+      <VStack w="1024px" p={32} alignItems="flex-start">
+        <Heading as="h1" id="contactme-section">
+          Contact me
+        </Heading>
+        <Box p={6} rounded="md" w="100%">
+          <form onSubmit={formik.handleSubmit}>
+            <VStack spacing={4}>
+              <FormControl isInvalid={formik.touched.firstName && formik.errors.firstName}>
+                <FormLabel htmlFor="firstName">Name</FormLabel>
+                <Input
+                  id="firstName"
+                  name="firstName"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <FormErrorMessage>Required</FormErrorMessage>
+
+              </FormControl>
+              <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+                <FormLabel htmlFor="email">Email Address</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <FormErrorMessage>Required</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={false}>
+                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                <Select
+                  id="type"
+                  name="type"
+                  value={formik.values.type}
+                  onChange={(e) => {
+                    formik.setFieldValue('type', e.target.value);
+                  }
+                  }
+                  onBlur={formik.handleBlur}
+                  onSelect={formik.handleChange}
+                >
+                  <option value="hireMe">Freelance project proposal</option>
+                  <option value="openSource">
+                    Open source consultancy session
+                  </option>
+                  <option value="other">Other</option>
+                </Select>
+              </FormControl>
+              <FormControl isInvalid={formik.touched.comment && formik.errors.comment}>
+                <FormLabel htmlFor="comment">Your message</FormLabel>
+                <Textarea
+                  id="comment"
+                  name="comment"
+                  height={250}
+                  value={formik.values.comment}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <FormErrorMessage></FormErrorMessage>
+              </FormControl>
+              <Button isLoading={isLoading} type="submit" colorScheme="purple" width="full">
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </FullScreenSection>
+  );
+};
+
+export default LandingSection;
